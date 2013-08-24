a354457d9420bc0da80a5f507722a11d8b2ff8ce
sam <graphicsxp@gmail.com>
2013-08-24 04:19:47 -0700

    added Entity

---
+++ src/Photograph/PhotoBundle/Entity/Message.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: sam
+ * Date: 8/24/13
+ * Time: 4:19 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Photograph\PhotoBundle\Entity;
+
+
+class Message {
+
+}
